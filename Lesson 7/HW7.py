a2d36a406b7bdbc96848be9c9d886b28fd4e7948
Kos10ko <kos10ko.r@yandex.kz>
2023-02-23 22:40:50 +0600

    v7

--- Lesson 7/HW7.py
+++ Lesson 7/HW7.py
@@ -8,19 +8,17 @@
 
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    """
-    Выводит таблицу значений для указанной функции operation,
-    вычисленных для всех пар (i, j), где i и j от 1 до num_rows и num_columns соответственно.
-    """
-    # Создаем список списков для хранения значений таблицы
+
+
+
     table = [[None] * num_columns for _ in range(num_rows)]
 
-    # Вычисляем значение для каждой пары (i, j) и сохраняем его в таблицу
+
     for i in range(1, num_rows + 1):
         for j in range(1, num_columns + 1):
             table[i-1][j-1] = operation(i, j)
 
-    # Выводим таблицу
+
     for row in table:
         print('\t'.join(map(str, row)))
 
