c4ef5650acca9f119ae7afac44fcc9569e6a1bd5
Kos10ko <kos10ko.r@yandex.kz>
2023-02-23 22:36:41 +0600

    v7

--- Lesson 7/HW7.py
+++ Lesson 7/HW7.py
@@ -1,6 +1,27 @@
-words = input().split()
-vowels_count = list(map(lambda word: sum(map(lambda letter: letter.lower() in 'уеыаоэяию', word)), words))
-if all(x == vowels_count[0] for x in vowels_count):
-    print('Парам пам-пам')
-else:
-    print('Пам парам')
+# words = input().split()
+# vowels_count = list(map(lambda word: sum(map(lambda letter: letter.lower() in 'уеыаоэяию', word)), words))
+# if all(x == vowels_count[0] for x in vowels_count):
+#     print('Парам пам-пам')
+# else:
+#     print('Пам парам')
+
+
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    """
+    Выводит таблицу значений для указанной функции operation,
+    вычисленных для всех пар (i, j), где i и j от 1 до num_rows и num_columns соответственно.
+    """
+    # Создаем список списков для хранения значений таблицы
+    table = [[None] * num_columns for _ in range(num_rows)]
+
+    # Вычисляем значение для каждой пары (i, j) и сохраняем его в таблицу
+    for i in range(1, num_rows + 1):
+        for j in range(1, num_columns + 1):
+            table[i-1][j-1] = operation(i, j)
+
+    # Выводим таблицу
+    for row in table:
+        print('\t'.join(map(str, row)))
+
+print_operation_table(lambda x, y: x * y)
